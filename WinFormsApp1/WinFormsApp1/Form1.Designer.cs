df53eeb122f11146f659c6b98c9689d9f8e1982b
Vlad-Ambr <Ambr_do13@student.itstep.org>
2023-07-10 19:34:42 +0300

    Last commid

--- WinFormsApp1/WinFormsApp1/Form1.Designer.cs
+++ WinFormsApp1/WinFormsApp1/Form1.Designer.cs
@@ -28,12 +28,166 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            menuStrip1 = new MenuStrip();
+            toolStripMenuItem1 = new ToolStripMenuItem();
+            updateListToolStripMenuItem = new ToolStripMenuItem();
+            toolStripMenuItem2 = new ToolStripMenuItem();
+            toolStripMenuItem3 = new ToolStripMenuItem();
+            ukranianToolStripMenuItem = new ToolStripMenuItem();
+            spanishToolStripMenuItem = new ToolStripMenuItem();
+            germanToolStripMenuItem = new ToolStripMenuItem();
+            frenchToolStripMenuItem = new ToolStripMenuItem();
+            panel1 = new Panel();
+            label2 = new Label();
+            panel2 = new Panel();
+            label1 = new Label();
+            panel3 = new Panel();
+            label3 = new Label();
+            menuStrip1.SuspendLayout();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            panel3.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { toolStripMenuItem1, updateListToolStripMenuItem, toolStripMenuItem2 });
+            resources.ApplyResources(menuStrip1, "menuStrip1");
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.RenderMode = ToolStripRenderMode.System;
+            menuStrip1.ItemClicked += menuStrip1_ItemClicked;
+            // 
+            // toolStripMenuItem1
+            // 
+            toolStripMenuItem1.Name = "toolStripMenuItem1";
+            resources.ApplyResources(toolStripMenuItem1, "toolStripMenuItem1");
+            toolStripMenuItem1.Click += toolStripMenuItem1_Click;
+            // 
+            // updateListToolStripMenuItem
+            // 
+            updateListToolStripMenuItem.Name = "updateListToolStripMenuItem";
+            resources.ApplyResources(updateListToolStripMenuItem, "updateListToolStripMenuItem");
+            updateListToolStripMenuItem.Click += updateListToolStripMenuItem_Click;
+            // 
+            // toolStripMenuItem2
+            // 
+            toolStripMenuItem2.Checked = true;
+            toolStripMenuItem2.CheckState = CheckState.Checked;
+            toolStripMenuItem2.DropDownItems.AddRange(new ToolStripItem[] { toolStripMenuItem3, ukranianToolStripMenuItem, spanishToolStripMenuItem, germanToolStripMenuItem, frenchToolStripMenuItem });
+            toolStripMenuItem2.Name = "toolStripMenuItem2";
+            resources.ApplyResources(toolStripMenuItem2, "toolStripMenuItem2");
+            toolStripMenuItem2.Click += toolStripMenuItem2_Click;
+            // 
+            // toolStripMenuItem3
+            // 
+            toolStripMenuItem3.Name = "toolStripMenuItem3";
+            resources.ApplyResources(toolStripMenuItem3, "toolStripMenuItem3");
+            toolStripMenuItem3.Click += toolStripMenuItem3_Click;
+            // 
+            // ukranianToolStripMenuItem
+            // 
+            ukranianToolStripMenuItem.Name = "ukranianToolStripMenuItem";
+            resources.ApplyResources(ukranianToolStripMenuItem, "ukranianToolStripMenuItem");
+            ukranianToolStripMenuItem.Click += ukranianToolStripMenuItem_Click;
+            // 
+            // spanishToolStripMenuItem
+            // 
+            spanishToolStripMenuItem.Name = "spanishToolStripMenuItem";
+            resources.ApplyResources(spanishToolStripMenuItem, "spanishToolStripMenuItem");
+            spanishToolStripMenuItem.Click += spanishToolStripMenuItem_Click;
+            // 
+            // germanToolStripMenuItem
+            // 
+            germanToolStripMenuItem.Name = "germanToolStripMenuItem";
+            resources.ApplyResources(germanToolStripMenuItem, "germanToolStripMenuItem");
+            germanToolStripMenuItem.Click += germanToolStripMenuItem_Click;
+            // 
+            // frenchToolStripMenuItem
+            // 
+            frenchToolStripMenuItem.Name = "frenchToolStripMenuItem";
+            resources.ApplyResources(frenchToolStripMenuItem, "frenchToolStripMenuItem");
+            frenchToolStripMenuItem.Click += frenchToolStripMenuItem_Click;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.GradientInactiveCaption;
+            panel1.BorderStyle = BorderStyle.Fixed3D;
+            panel1.Controls.Add(label2);
+            resources.ApplyResources(panel1, "panel1");
+            panel1.Name = "panel1";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(label2, "label2");
+            label2.Name = "label2";
+            // 
+            // panel2
+            // 
+            panel2.BackColor = SystemColors.GradientInactiveCaption;
+            panel2.BorderStyle = BorderStyle.Fixed3D;
+            panel2.Controls.Add(label1);
+            resources.ApplyResources(panel2, "panel2");
+            panel2.Name = "panel2";
+            // 
+            // label1
+            // 
+            resources.ApplyResources(label1, "label1");
+            label1.Name = "label1";
+            // 
+            // panel3
+            // 
+            panel3.BackColor = SystemColors.GradientInactiveCaption;
+            panel3.BorderStyle = BorderStyle.Fixed3D;
+            panel3.Controls.Add(label3);
+            resources.ApplyResources(panel3, "panel3");
+            panel3.Name = "panel3";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(label3, "label3");
+            label3.Name = "label3";
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(panel3);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "Form1";
+            Load += Form1_Load;
+            SizeChanged += Form1_SizeChanged;
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            panel3.ResumeLayout(false);
+            panel3.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem toolStripMenuItem1;
+        private ToolStripMenuItem toolStripMenuItem2;
+        private ToolStripMenuItem toolStripMenuItem3;
+        private ToolStripMenuItem updateListToolStripMenuItem;
+        private ToolStripMenuItem ukranianToolStripMenuItem;
+        private ToolStripMenuItem spanishToolStripMenuItem;
+        private ToolStripMenuItem germanToolStripMenuItem;
+        private ToolStripMenuItem frenchToolStripMenuItem;
+        private Panel panel1;
+        private Label label2;
+        private Panel panel2;
+        private Label label1;
+        private Panel panel3;
+        private Label label3;
     }
 }
--- WinFormsApp1/WinFormsApp1/Form1.cs
+++ WinFormsApp1/WinFormsApp1/Form1.cs
@@ -1,10 +1,104 @@
+using System.ComponentModel;
+using System.Data.SqlClient;
+using System.Globalization;
+
 namespace WinFormsApp1
 {
+
     public partial class Form1 : Form
     {
+        int y = 50;
+        int countUser = 1;
         public Form1()
         {
             InitializeComponent();
         }
+        private void Download_DataBase()
+        {
+            using (SqlConnection connection = new SqlConnection("Server=HOME;Database=TestUserDB;Trusted_Connection=True;TrustServerCertificate=True;"))
+            {
+                connection.Open();
+                SqlCommand cmd = new SqlCommand(@"SELECT * FROM [USER]", connection);
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    UserControl1 item = new UserControl1(int.Parse(reader[0].ToString()), reader[1].ToString(), reader[2].ToString());
+                    item.Name = $"User_{countUser++}";
+                    item.Location = new Point(0, y);
+                    this.Controls.Add(item);
+                    y += item.Height - 1;
+                }
+            }
+            y = 50;
+        }
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void toolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Download_DataBase();
+        }
+        private void Change_Language(string lang)
+        {
+            ComponentResourceManager manager = new ComponentResourceManager(typeof(Form1));
+            CultureInfo culture = new CultureInfo(lang);
+            foreach (Control item in this.Controls)
+                manager.ApplyResources(item, item.Name, culture);
+        }
+
+        private void toolStripMenuItem3_Click(object sender, EventArgs e)
+        {
+            Change_Language("en");
+        }
+
+        private void ukranianToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Change_Language("uk");
+        }
+
+        private void spanishToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Change_Language("es");
+        }
+
+        private void germanToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Change_Language("de");
+        }
+
+        private void frenchToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Change_Language("fr");
+        }
+        private void updateListToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            while (countUser != 0)
+            {
+                this.Controls.RemoveByKey($"User_{countUser--}");
+            }
+            countUser = 1;
+            Download_DataBase();
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            Form2 newForm = new Form2();
+            newForm.Show();
+            newForm.FormClosed += updateListToolStripMenuItem_Click;
+            newForm.FormClosed += Form1_Load;
+        }
+
     }
 }
